589e4b5fc12231895163b659e2a7e8b734c31f4b
JuliusA <julius.soltani@gmail.com>
2021-11-12 02:25:16 +0100

    added a module

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 "use strict";
 
-const test = "Du är en apa";
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+}
